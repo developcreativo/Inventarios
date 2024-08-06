690e847ad8e544d7994089acbf21dbb840896c24
Kelly Salazar <developmentcreativo@gmail.com>
2024-08-06 18:47:13 -0400

    Module

--- databases/migrations/2024_07_27_005019_create_equipment_table.php
+++ databases/migrations/2024_07_27_005019_create_equipment_table.php
@@ -3,23 +3,27 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Doctrine\DBAL\Types\FloatType;
+use Doctrine\DBAL\Types\Type;
 class CreateEquipmentTable extends Migration
 {
     public function up(): void
     {
+        if (!Type::hasType('double')) {
+            Type::addType('double', FloatType::class);
+        }
         Schema::create('equipment', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('comments');
             $table->integer('equipment_type');
-            $table->double('avg_price');
-            $table->integer('available_items');
+            $table->double('avg_price', 15, 2)->nullable()->default(0);
+            $table->unsignedBigInteger('available_items')->nullable()->default(0);
             $table->double('items_value');
             $table->integer('last_order_id');
             $table->integer('reorder_point');
             $table->boolean('reorder_flag');
-            $table->unsignedBigInteger('equipment_talla_id')->nullable()->default(null);
+            $table->unsignedBigInteger('id_talla')->nullable()->default(null);
             $table->timestamps();
         });
     }
--- databases/migrations/2024_07_27_005720_create_equipment_orders_table.php
+++ databases/migrations/2024_07_27_005720_create_equipment_orders_table.php
@@ -3,11 +3,16 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Doctrine\DBAL\Types\FloatType;
+use Doctrine\DBAL\Types\Type;
 class CreateEquipmentOrdersTable extends Migration
 {
     public function up(): void
     {
+        if (!Type::hasType('double')) {
+            Type::addType('double', FloatType::class);
+        }
+
         Schema::create('equipment_orders', function (Blueprint $table) {
             $table->id();
             $table->dateTime('order_date');
@@ -15,12 +20,12 @@ class CreateEquipmentOrdersTable extends Migration
             $table->integer('id_equipment');
             $table->integer('quantity');
             $table->double('order_price')->nullable()->default(0);
-            $table->integer('available_items_before');
-            $table->integer('available_items_after');
+            $table->integer('available_items_before')->nullable()->default(0);
+            $table->integer('available_items_after')->nullable()->default(0);
             $table->integer('user_id');
             $table->string('id_usuario')->nullable()->default(null);
             $table->text('comments');
-            $table->unsignedBigInteger('talla_id')->nullable()->default(null);
+            $table->unsignedBigInteger('equipo_talla_id')->nullable()->default(null);
             $table->timestamps();
         });
     }
